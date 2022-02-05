fcacd63670da07aed4237f09ffe36538d84ebc3b
JesusBL24 <jesusbaslop24@gmail.com>
2022-02-05 16:36:15 +0100

    Adicion de Archer

--- Assets/Scripts/Enemigo/Enemy.cs
+++ Assets/Scripts/Enemigo/Enemy.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed;
-    public float sttoppingDistance;
     public float retreatDistance;
+    public float shotingDistance;
+    public float detectionDistance;
 
     private float timeBtwShots;
     public float startTimeBtwShots;
 
     public Transform player;
+    public Transform puntero;
     public GameObject projectile;
     void Start()
     {
@@ -22,18 +24,18 @@ public class Enemy : MonoBehaviour
 
     
     void Update()
-    {
-        if(Vector2.Distance(transform.position, player.position)> sttoppingDistance){
-            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
-           
-        }else if(Vector2.Distance(transform.position, player.position) < sttoppingDistance && Vector2.Distance(transform.position, player.position)> retreatDistance ){
-            transform.position = this.transform.position;
+    {  
+        if( Vector2.Distance(transform.position, player.position)> retreatDistance){
+           transform.position = this.transform.position;
         }else if(Vector2.Distance(transform.position, player.position)< retreatDistance){
             transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }else if (Vector2.Distance(transform.position, player.position)< detectionDistance){
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
         }
 
-        if(timeBtwShots <= 0){
-            Instantiate(projectile, transform.position, Quaternion.identity );
+        if(timeBtwShots <= 0 && Vector2.Distance(transform.position, player.position)< shotingDistance){
+            Instantiate(projectile, puntero.position, Quaternion.identity );
+
             timeBtwShots = startTimeBtwShots;   
         }else{
             timeBtwShots -= Time.deltaTime;
