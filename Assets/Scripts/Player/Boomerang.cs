8f6ed3fb04794e570e961dd29b7dc669288782a9
JesusBL24 <jesusbaslop24@gmail.com>
2022-02-06 12:07:12 +0100

    Jesus update mañana

--- Assets/Scripts/Player/Boomerang.cs
+++ Assets/Scripts/Player/Boomerang.cs
@@ -8,11 +8,9 @@ public class Boomerang : MonoBehaviour
    public float tiempoBala;
    
    public Rigidbody2D rb;
-
    public float contador;
    private float activoContador;
    public float tiempoVolver;
-   private bool atras;
 
    private Vector2 moveDirection;
    private Vector2 returnMoveDirection;
@@ -23,7 +21,6 @@ public class Boomerang : MonoBehaviour
 
    void Start(){
        activoContador = contador;
-       atras = false;
        rb = GetComponent<Rigidbody2D>();
        player = GameObject.FindGameObjectWithTag("Player").transform;
        moveDirection = new Vector2(speed, 0);
@@ -32,25 +29,16 @@ public class Boomerang : MonoBehaviour
    }
    void Update(){
        if (activoContador <= 0){
-         //moveDirection = new Vector2(-speed,0).normalized;
-         //rb.velocity = new Vector2(returnMoveDirection.x*speed, returnMoveDirection.y*speed);
          Retroceso();
-         atras = true;
-         //activoContador = contador;
        }else{
           activoContador--;
        } 
    }
-    void FixedUpdate(){
-        if (atras){
-           rb.velocity = new Vector2(returnMoveDirection.x*speed, returnMoveDirection.y*speed);
-    }
-   }
+
+   
    void Retroceso(){
-       //returnMoveDirection = new Vector2((player.position.x - rb.position.x), (player.position.x - rb.position.x)).normalized;
-       //moveDirection = new Vector2(-speed*2,0 ).normalized;
-       rb.velocity = new Vector2(0,0);
-       moveDirection = new Vector2(speed*(-2),0 ).normalized;
+       rb.velocity = new Vector2(-speed*3,0);
+       
    }
    
    void OnTriggerEnter2D(Collider2D other){
